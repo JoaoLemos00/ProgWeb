719340ff3544ee73c77a5dd89143a8b66a7518ec
JoaoLemos00 <joao.lemos2703@gmail.com>
2023-08-23 19:00:44 -0300

    v1

--- venv/MeuSite/contatos/urls.py
+++ venv/MeuSite/contatos/urls.py
@@ -1,2 +1,11 @@
+from django.urls.conf import path
+from contatos import views
 
-urlpatterns = []
+app_name = "contatos"
+
+urlpatterns = [
+
+    path('lista/', views.ContatoListView.as_view(),name='lista-contatos'),
+    path('', views.ContatoListView.as_view(),name='home-contatos'),
+    
+]
