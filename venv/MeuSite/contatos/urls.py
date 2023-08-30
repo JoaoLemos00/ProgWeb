0b46884963665a00df4c6eeeaa2443ae443d9132
JoaoLemos00 <joao.lemos2703@gmail.com>
2023-08-30 18:24:50 -0300

    v1

--- venv/MeuSite/contatos/urls.py
+++ venv/MeuSite/contatos/urls.py
@@ -5,9 +5,9 @@ app_name = "contatos"
 
 urlpatterns = [
 
-    path('/lista', views.ContatoListView.as_view(),name='lista-contatos'),
+    path('lista/', views.ContatoListView.as_view(),name='lista-contatos'),
     path('', views.ContatoListView.as_view(), name='home-contatos'),
-    path('/cria', views.ContatoCreateView.as_view(),name='cria-contato'),
+    path('cria/', views.ContatoCreateView.as_view(),name='cria-contato'),
 
     
 ]
